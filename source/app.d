e77c43727195d061eb9f75e6fa01d5cfd1c1e550
Daniel Munch <daniellmunch@gmail.com>
2020-12-23 11:35:57 +0100

    fixed app.d indentation

--- source/app.d
+++ source/app.d
@@ -3,43 +3,43 @@ import viva.types.string;
 
 private void handleError(HTTPServerRequest _, HTTPServerResponse res, HTTPServerErrorInfo error)
 {
-	import std.conv : to;
+    import std.conv : to;
 
-	string errorDebug = "";
-	debug errorDebug = error.debugMessage;
+    string errorDebug = "";
+    debug errorDebug = error.debugMessage;
 
-	res.contentType = "application/json";
-	res.writeBody(str(
-		`{"code": "`, error.code.to!string,
-		`", "status": "`, error.message,
-		`", "message": "`, error.debugMessage,
-		`"}`
-	));
+    res.contentType = "application/json";
+    res.writeBody(str(
+        `{"code": "`, error.code.to!string,
+        `", "status": "`, error.message,
+        `", "message": "`, error.debugMessage,
+        `"}`
+    ));
 }
 
 public void main()
 {
-	import backend.rest : AccountsAPI, ServersAPI, UsersAPI, MeAPI;
-	import backend.data : config;
-	import backend.db : connectDB;
+    import backend.rest : AccountsAPI, ServersAPI, UsersAPI, MeAPI;
+    import backend.data : config;
+    import backend.db : connectDB;
 
-	URLRouter router = new URLRouter();
+    URLRouter router = new URLRouter();
 
-	router.registerRestInterface(new AccountsAPI());
-	router.registerRestInterface(new ServersAPI());
-	router.registerRestInterface(new UsersAPI());
-	router.registerRestInterface(new MeAPI());
+    router.registerRestInterface(new AccountsAPI());
+    router.registerRestInterface(new ServersAPI());
+    router.registerRestInterface(new UsersAPI());
+    router.registerRestInterface(new MeAPI());
 
-	HTTPServerSettings serverSettings = new HTTPServerSettings();
-	serverSettings.bindAddresses = [config.hostIp];
-	serverSettings.port = config.hostPort;
+    HTTPServerSettings serverSettings = new HTTPServerSettings();
+    serverSettings.bindAddresses = [config.hostIp];
+    serverSettings.port = config.hostPort;
     serverSettings.sessionStore = new MemorySessionStore();
-	serverSettings.errorPageHandler = toDelegate(&handleError);
-	
-	connectDB();
+    serverSettings.errorPageHandler = toDelegate(&handleError);
+    
+    connectDB();
 
-	listenHTTP(serverSettings, router);
-	//listenTCP(config.socketPort, &handler, config.socketHost);
+    listenHTTP(serverSettings, router);
+    //listenTCP(config.socketPort, &handler, config.socketHost);
 
-	runApplication();
+    runApplication();
 }
